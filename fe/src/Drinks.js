bbb28b94b2127dbddefe958f22d9eed2f7bbaaf4
Ross Savill <rosssavill252@hotmail.com>
2020-08-22 11:04:37 +0100

    table in progress

--- fe/src/Drinks.js
+++ fe/src/Drinks.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const drinksImport = axios.create({
+  baseURL: 'http://localhost:5000/drinks',
+})
+
 class Drinks extends Component {
+  constructor(props) {
+    super(props)
+      this.state = { drinks: [] }
+  }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/drinks')
-      .then(resp => console.log(resp.data))
+    drinksImport.get()
+      .then(drinks => this.setState({ drinks: drinks }))
+      .catch(error => console.log(error))
   }
 
-  componentWillUpdate() {
+  componentDidUpdate(prevProps, prevState) {
 
-  }
+    }
+
+    renderTableHeader = () => {
+      let header = Object.keys(this.state.drinks[0])
+      return header.map((key, index) => {
+         return <th key={index}>{key}</th>
+      })
+    }
+
+     renderTableData = () => {
+        return this.state.drinks.map((drink, index) => {
+           const { name, date, drinkMain, drinkType, mixerOne, mixerTwo, garnish, ratingWordOne, ratingWordTwo, score, brand, collabOne, collabTwo, company} = drink
+           return (
+              <tr key={index}>
+                 <td>{name}</td>
+                 <td>{date}</td>
+                 <td>{drinkMain}</td>
+                 <td>{drinkType}</td>
+                 <td>{mixerOne}</td>
+                 <td>{mixerTwo}</td>
+                 <td>{garnish}</td>
+                 <td>{ratingWordOne}</td>
+                 <td>{ratingWordTwo}</td>
+                 <td>{score}</td>
+                 <td>{brand}</td>
+                 <td>{collabOne}</td>
+                 <td>{collabTwo}</td>
+                 <td>{company}</td>
+              </tr>
+           )
+        })
+     }
 
   render() {
-    return (
-      <button>Click me</button>
-    )
+    if(this.state.drinks === []) {
+      return <h1>Please Wait</h1>
+    } else {
+      const { drinks } = this.state
+      { console.log(this.state) }
+
+
+      return (
+        <div className="container">
+          <h1>Drinks Table</h1>
+          <table className="mainTable">
+            <tbody>
+              <tr>{this.renderTableHeader()}</tr>
+              {this.renderTableData()}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
   }
 }
 

07d9dcbe864e583da418ddf6b3d2211785307add
Ross Savill <rosssavill252@hotmail.com>
2020-09-03 01:46:43 +0100

    Table component now contains main data, Drinks and Search components deleted

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,19 +1,128 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
-import Drinks from './Drinks';
 import Search from './Search';
+import Table from './Table';
+import axios from 'axios';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Dates = ({ values }) => {
+  return values.slice(0,10)
+}
 
-class App extends Component {
-  render() {
+const Scores = ({ values }) => {
+  if(values > 7.5) {
     return (
-    <BrowserRouter>
-      <Route exact path="/" component={Drinks}></Route>
-      <Route exact path="/search" component={Search}></Route>
-    </BrowserRouter>
+      <span className="goodBadge">
+        {values}
+      </span>
+    )
+  } else if (values <= 7.5 && values >= 4) {
+    return (
+      <span className="averageBadge">
+        {values}
+      </span>
+    )
+  } else {
+    return (
+      <span className="badBadge">
+        {values}
+      </span>
     )
   }
 }
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const allDrinks = await axios("http://localhost:5000/drinks");
+      setData(allDrinks.data);
+    })();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "All Drinks",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name"
+          },
+          {
+            Header: "Date",
+            accessor: "date",
+            Cell: ({ cell: { value } }) => <Dates values={value} />
+          },
+          {
+            Header: "Main Component",
+            accessor: "drinkMain"
+          },
+          {
+            Header: "Drink Type",
+            accessor: "drinkType"
+          },
+          {
+            Header: "Brand or Brewery",
+            accessor: "brand"
+          },
+          {
+            Header: "Mixer One",
+            accessor: "mixerOne"
+          },
+          {
+            Header: "Mixer Two",
+            accessor: "mixerTwo"
+          },
+          {
+            Header: "Garnish",
+            accessor: "garnish"
+          },
+          {
+            Header: "Rating Word One",
+            accessor: "ratingWordOne"
+          },
+          {
+            Header: "Rating Word Two",
+            accessor: "ratingWordTwo"
+          },
+          {
+            Header: "Score",
+            accessor: "score",
+            Cell: ({ cell: { value } }) => <Scores values={value} />
+          },
+          {
+            Header: "Collaborator",
+            accessor: "collabOne"
+          },
+          {
+            Header: "Additional Collaborator",
+            accessor: "collabTwo"
+          },
+          {
+            Header: "Company",
+            accessor: "company"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+    return (
+      <div>
+        <div className="App">
+          <Table columns={columns} data={data} />
+        </div>
+
+        <BrowserRouter>
+          <Route exact path="/search" component={Search}></Route>
+          <Route exact path="/table" component={Table}></Route>
+        </BrowserRouter>
+      </div>
+    )
+  }
 
 export default App;
---
+++ fe/src/Table.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { useTable, useFilters, useSortBy, usePagination } from "react-table";
+
+export default function Table({ columns, data }) {
+
+  const [filterInput, setFilterInput] = useState("");
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    gotoPage,
+    nextPage,
+    previousPage,
+    pageOptions,
+    pageCount,
+    canPreviousPage,
+    canNextPage,
+    setPageSize,
+    prepareRow,
+    setFilter,
+    state: { pageIndex, pageSize }
+  } = useTable({
+    columns,
+    data
+  },
+    useFilters,
+    useSortBy,
+    usePagination
+  );
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("drinkMain", value);
+    setFilterInput(value);
+  };
+
+  return (
+    <>
+      <p>Search for a Drink:{' '}
+        <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search Drink Name"}
+        />
+      </p>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[20, 40, 60, 80, 100].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+        {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={
+                    column.isSorted
+                      ? column.isSortedDesc
+                        ? "sort-desc"
+                        : "sort-asc"
+                      : ""
+                  }
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}

ec7af60caf70b45769511df7932de06bb6535786
Ross Savill <rosssavill252@hotmail.com>
2020-11-04 21:20:26 +0000

    countries now displayed

--- fe/src/MapFolder/Maps.js
+++ fe/src/MapFolder/Maps.js
@@ -15,32 +15,38 @@ class Maps extends Component {
   componentDidUpdate(prevProps, prevState) {
     const currentPropDrinks = this.props.drinks
     const currentPropDrinkers = this.props.drinkers
-    if(currentPropDrinks !== this.state.drinks) {
-      this.setState({ drinks: currentPropDrinks,
-                      drinkers: currentPropDrinkers })
-    }
+    // if(currentPropDrinks !== this.state.drinks) {
+    //   this.setState({ drinks: currentPropDrinks,
+    //                   drinkers: currentPropDrinkers })
+    // }
       let countryData = []
-      const { drinks } = this.state
-      drinks.map((drink) => {
-        if(drink.secondCollabCountry) {
-          countryData.push(drink.secondCollabCountry, drink.firstCollabCountry, drink.country)
-        } else if (drink.firstCollabCountry) {
-          countryData.push(drink.firstCollabCountry, drink.country)
-        } else {
-          countryData.push(drink.country)
-        }
-      })
-      this.setState({ mapData: countryData })
-
+      currentPropDrinks
+        .filter(drink => drink.country !== "Barbados" && drink.country !== "-")
+        .map((drink) => {
+          if(drink.secondCollabCountry) {
+            countryData.push(drink.secondCollabCountry, drink.firstCollabCountry, drink.country)
+          } else if (drink.firstCollabCountry) {
+            countryData.push(drink.firstCollabCountry, drink.country)
+          } else {
+            countryData.push(drink.country)
+          }
+        })
       //TURN THE ABOVE INTO AN OBJECT OF COUNTRY CODES AND NUMBER OF APPEARANCES AS BELOW (not object bit)
 
-      // const countedUniqueDrinkers = whoSaidIt.reduce(function(occ, name) {
-      //   occ[name] = (occ[name] || 0) + 1;
-      //   return occ;
-      // }, {});
-      // const useableDrinkerData = [];
-      // for(const name in countedUniqueDrinkers) {
-      //   useableDrinkerData.push([name, countedUniqueDrinkers[name]]);
+      const countedUniqueCountries = countryData.reduce(function(occ, name) {
+        occ[name] = (occ[name] || 0) + 1;
+        return occ;
+      }, {});
+
+      if(currentPropDrinks !== this.state.drinks) {
+        this.setState({ drinks: currentPropDrinks,
+                        drinkers: currentPropDrinkers,
+                        mapData: countedUniqueCountries
+                      })
+        }
+
+      // if(this.state.mapData !== countedUniqueCountries) {
+      //   this.setState({ mapData: countedUniqueCountries })
       // }
   }
 
@@ -49,48 +55,52 @@ class Maps extends Component {
   };
 
   render() {
-    return(
-      <div>
-        <VectorMap
-          map={"world_mill"}
-          backgroundColor="transparent" //change it to ocean blue: #0077be
-          zoomOnScroll={false}
-          containerStyle={{
-            width: "100%",
-            height: "520px"
-          }}
-          onRegionClick={this.handleClick} //gets the country code
-          containerClassName="map"
-          regionStyle={{
-            initial: {
-              fill: "#e4e4e4",
-              "fill-opacity": 0.9,
-              stroke: "none",
-              "stroke-width": 0,
-              "stroke-opacity": 0
-            },
-            hover: {
-              "fill-opacity": 0.8,
-              cursor: "pointer"
-            },
-            selected: {
-              fill: "#2938bc" //color for the clicked country
-            },
-            selectedHover: {}
-          }}
-          regionsSelectable={true}
-          series={{
-            regions: [
-              {
-                values: mapData, //this is your data
-                scale: ["#146804", "#ff0000"], //your color game's here
-                normalizeFunction: "polynomial"
-              }
-            ]
-          }}
-        />
-      </div>
-    )
+    if(!this.state.mapData) {
+      return("Please Wait")
+    } else {
+      return(
+        <div>
+          <VectorMap
+            map={"world_mill"}
+            backgroundColor="transparent" //change it to ocean blue: #0077be
+            zoomOnScroll={false}
+            containerStyle={{
+              width: "100%",
+              height: "520px"
+            }}
+            onRegionClick={this.handleClick} //gets the country code
+            containerClassName="map"
+            regionStyle={{
+              initial: {
+                fill: "#e4e4e4",
+                "fill-opacity": 0.9,
+                stroke: "none",
+                "stroke-width": 0,
+                "stroke-opacity": 0
+              },
+              hover: {
+                "fill-opacity": 0.8,
+                cursor: "pointer"
+              },
+              selected: {
+                fill: "#2938bc" //color for the clicked country
+              },
+              selectedHover: {}
+            }}
+            regionsSelectable={true}
+            series={{
+              regions: [
+                {
+                  values: this.state.mapData, //this is your data
+                  scale: ["#146804", "#ff0000"], //your color game's here
+                  normalizeFunction: "polynomial"
+                }
+              ]
+            }}
+          />
+        </div>
+      )
+    }
   }
 };
 
